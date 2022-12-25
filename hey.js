d441ab14b63b02e6435e2ad06f875ea0dad9ebbe
sobanx <98476274+sobanx@users.noreply.github.com>
2022-12-26 01:01:52 +0500

    Add files via upload

---
+++ hey.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>;
+var typed = new typed(".auto-type", {
+  strings: ["Rohan", "Kashaf", "Ilsa"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
